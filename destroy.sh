a65b06a1509ede5235c55d4481832d7e6a6882d3
jkmathes <jack@jkmathes.org>
2016-07-29 22:35:46 -0500

    Add missing var to destroy script

--- destroy.sh
+++ destroy.sh
@@ -3,4 +3,5 @@ terraform destroy -var "do_api_key=${DO_API_KEY}" \
   -var "deploy_private_key=./deploy_rsa" \
   -var "public_key=./do_rsa.pub" \
   -var "private_key=./do_rsa" \
-  -var "deploy_passwd=${DEPLOY_PASSWD}"
+  -var "deploy_passwd=${DEPLOY_PASSWD}" \
+  -var "external_ip=${EXTERNAL_IP}"
